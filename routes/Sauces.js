b678f1a83fc2856852f80ca41c5bd59208edbbca
Camille Martignac <79940961+Arev-tech@users.noreply.github.com>
2021-09-29 15:06:44 +0200

    sauces

--- routes/Sauces.js
+++ routes/Sauces.js
@@ -1,13 +1,15 @@
 const express = require ('express');
+
 const router = express.Router();
+const sauceCtrl = require('../controllers/Sauces');
 
-const saucesCtrl = require('../controllers/Sauces');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, saucesCtrl.createSauce);
-router.put('/:id', auth, saucesCtrl.modifySauce);
-router.delete('/:id', auth, saucesCtrl.deleteSauce);
-router.get('/:id', auth, saucesCtrl.getOneSauce);
-router.get('/', auth, saucesCtrl.getAllSauces);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
